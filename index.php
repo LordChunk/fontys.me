46066788ef6d7daa090641671f2fec4457642527
Job <Rubixsphere@protonmail.com>
2017-09-11 20:58:12 +0200

    Updated files so they now work on a dev version of the website.

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 		<!-- Below fold -->
 		<div class="container">
 			<h1>Dit is een work in progress build van <a href="http://fontys.me">fontys.me</a>. </h1>
-
+            <p>Verwacht bugs enzo. Ik ben niet perfect.</p>
 	
 		</div>
 	</body>
