8737348c67008fc10153e8983a974298ddee4a0f
Job <Rubixsphere@protonmail.com>
2017-09-13 17:33:41 +0200

    Updated foto_query to support the show boolean in the database

--- media/includes/foto_query.inc.php
+++ media/includes/foto_query.inc.php
@@ -22,28 +22,30 @@ if (mysqli_num_rows($result) > 0) //Check if database has input
 
 if (mysqli_num_rows($result) > 0) 
 {
-	while ($row = mysqli_fetch_assoc($result)) 
-	{ ?>
-		<li>
-	    	<div class="collapsible-header"><i class="material-icons">insert_photo</i><?php echo $row["banner_naam"]; ?></div>
-	    	<div class="collapsible-body no-padding">
-				<div class="card no-margin">
+	while ($row = mysqli_fetch_assoc($result))
+	{
+	    if ($row["show"])
+	    {?>
+            <li>
+                <div class="collapsible-header"><i class="material-icons">insert_photo</i><?php echo $row["banner_naam"]; ?></div>
+                <div class="collapsible-body no-padding">
+                    <div class="card no-margin">
 
-		            <div class="card-image">
-		            	<img src="<?php echo $row["foto_url"]; ?>" alt="<?php echo $row["banner_naam"]; ?> image" >
-		            </div>
-		        
-		            <div class="card-content">
-		            	<p><?php echo $row["tekst"]; ?></p>
-		            </div>
-		        
-		            <div class="card-action">
-		            	<a href="<?php echo $row["foto_url"]; ?>" download="<?php echo $row["foto_url"]; ?>">Download</a>
-		            </div>
-	        	</div>
-	    	</div>
-	    </li>
-<?php	
+                        <div class="card-image">
+                            <img src="<?php echo $row["foto_url"]; ?>" alt="<?php echo $row["banner_naam"]; ?> image" >
+                        </div>
+
+                        <div class="card-content">
+                            <p><?php echo $row["tekst"]; ?></p>
+                        </div>
+
+                        <div class="card-action">
+                            <a href="<?php echo $row["foto_url"]; ?>" download="<?php echo $row["foto_url"]; ?>">Download</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+<?php   }
 	}
 } else 
 {
