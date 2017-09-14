b8cac1d2baaa6776e1bfd72f5ad7e714e0578d9e
Job <Rubixsphere@protonmail.com>
2017-09-14 13:00:55 +0200

    Added subdomain file

--- subdomain/index.php
+++ subdomain/index.php
@@ -1,9 +1,66 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Job
- * Date: 14/09/2017
- * Time: 09:29
- */
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="https://materialize.van-ooik.nl/css/materialize.min.css"  media="screen,projection"/>
 
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+
+    <!--Logos-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <!--Files I didn't find on the dark web-->
+    <link rel="stylesheet" type="text/css" href="../media/css/main.css">
+
+
+
+    <title>WIP Build</title>
+</head>
+
+
+<body>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="https://materialize.van-ooik.nl/js/materialize.min.js"></script>
+<!--My own javascript stuff -->
+<script src="../media/js/main.js" type="text/javascript"></script>
+
+<!--Above fold -->
+<div class="welcome">
+    <!--Navigation bar -->
+    <nav>
+        <div class="nav-wrapper">
+            <a href="../">WIP build dashboard</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+
+
+            <!--Normal nav bar -->
+            <ul class="right hide-on-med-and-down">
+                <li class="active"><a href="/subdomain">Sign up for fontys.me subdomain</a></li>
+                <li><a href="#">nav 3</a></li>
+                <li><a href="#">nav 4</a></li>
+            </ul>
+
+            <!--Hidden mobile nav -->
+            <ul class="side-nav" id="mobile-demo">
+                <li class="active"><a href="/subdomain">Sign up for fontys.me subdomain</a></li>
+                <li><a href="#">nav 3</a></li>
+                <li><a href="#">nav 4</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<!-- Below fold -->
+<div class="container">
+    <h1>Sign up for a free fontys.me subdomain </h1>
+    <p>You'll need to be required to login with your FHICT account first. You'll get one subdomain per account.</p>
+
+</div>
+</body>
+</html>
