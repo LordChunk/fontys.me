5717e1ae07c47f8e597728c143ee0b1ef3548f43
Job <Rubixsphere@protonmail.com>
2017-09-14 19:35:42 +0200

    Configured login test V1

--- subdomain.php
+++
@@ -1,9 +0,0 @@
-<?php include "media/includes/head.inc.php";?>
-<!-- Body -->
-<div class="container">
-    <h1>Sign up for a free fontys.me subdomain </h1>
-    <p>You'll need to be required to login with your FHICT account first. You'll get one subdomain per account.</p>
-
-</div>
-</body>
-</html>
