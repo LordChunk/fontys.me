52e40b1802bd28c36262c45253de2670dc35a207
Job <Rubixsphere@protonmail.com>
2017-09-15 19:57:35 +0200

    Setup subdomain user register settings

--- media/includes/subdomain_add.inc.php
+++ media/includes/subdomain_add.inc.php
@@ -5,3 +5,76 @@
  * Date: 15/09/2017
  * Time: 15:04
  */
+session_start();
+include "connect.inc.php";
+
+//Echo all client details
+/*
+?>
+<h1>Welcome <?=$_SESSION['name']?></h1>
+<ul>
+    <?php foreach($_SESSION as $key => $value){
+        echo "<li>$key => $value</li>";
+    }?>
+</ul>
+<br><br>
+<?php
+*/
+
+//This is easier to use in sql queries
+$email = $_SESSION['email'];
+
+//Check if user is already registered
+$sql = "SELECT email FROM main WHERE email='$email'";
+$result = mysqli_query($conn, $sql);
+
+//Get no. of rows (Should be 0 for unregistered user and 1 for registered user)
+$row = mysqli_num_rows($result);
+
+if($row == 0)
+{
+    $sql = "INSERT INTO main (email, max_domains) VALUES ('$email', 1)";
+    $result = mysqli_query($conn, $sql);
+} elseif ($row == 1)
+{
+    echo "User is registered";
+} else {
+    echo "Your account has been registered twice. <br>This usually is the result of some kind of bug. <br>Please contact the developer at: j.vanooik@student.fontys.nl";
+}
+
+
+
+
+
+
+/*Finally add the actual domain */
+/*include "cloudflare_api.inc.php";
+
+$data = [
+    //'{"type":"A","name":"example.com","content":"127.0.0.1","ttl":120,"proxied":false}'
+    "type" => "A",
+    "name" => $_POST["subdomein"],
+    "content" => $_POST["IP"],
+    "ttl" => '1',
+    "proxied" => false,
+    "X-Auth-Email" => "admin@jobvanooik.nl"
+];
+
+$headers = ["X-Auth-Email: ". $email, "X-Auth-Key: " . $token, "Content-Type: application/json"];
+
+$ch = curl_init("https://api.cloudflare.com/client/v4/zones/". $dnszone . "/dns_records");
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    exit('Error: ' . curl_error($ch));
+}
+
+
+//$json = json_decode($result, true)["errors"]/*[0]["message"]*/;
+//var_dump($json);
+//echo $json;
--- subdomain.php
+++ subdomain.php
@@ -6,15 +6,33 @@ if($_SESSION['ingelogd'] !== true){
 }include "media/includes/head.inc.php";?>
 <!-- Body -->
 <div class="container">
+    <!-- Old
     <form method="post" action="media/includes/subdomain_add.inc.php">
         <input type="text" placeholder="Domain"/>
         <input type="text" placeholder="IP (IPv4)"/>
-        <input type="number" placeholder="1"/>
+        <input type="number" placeholder="TTL"/>
         <button class="btn waves-effect waves-light" type="submit" name="action">Submit request
             <i class="material-icons right">send</i>
         </button>
-    </form>
+    </form> -->
+    <div class="input_field card-panel  ">
+        <h5 id="picture-add">Request a domain here:</h5>
+        <form action="/media/includes/subdomain_add.inc.php" method="POST">
 
+            <div class="input-field">
+                <input type="text" id="subdomein" name="subdomein" data-length="50">
+                <label for="subdomein">Subdomain</label>
+            </div>
+            <div class="input-field">
+                <input type="text" id="IP" name="IP">
+                <label for="IP">IP (IPv4)</label>
+            </div>
+
+            <button class="btn waves-effect waves-light" type="submit" name="action">Request domain
+                <i class="material-icons right">send</i>
+            </button>
+        </form>
+    </div>
 </div>
 </body>
 </html>
