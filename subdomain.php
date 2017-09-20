51522a84cabbd18129b3bc32e9696e3dad0ad6be
Job <Rubixsphere@protonmail.com>
2017-09-20 21:38:08 +0200

    Added mobile background and added some form CSS

--- subdomain.php
+++ subdomain.php
@@ -1,31 +1,22 @@
 <?php
 session_start();
 if($_SESSION['ingelogd'] !== true){
-    include "media/includes/login.inc.php";
+    //include "media/includes/login.inc.php";
 
 }include "media/includes/head.inc.php";?>
 <!-- Body -->
 <div class="container">
-    <!-- Old
-    <form method="post" action="media/includes/subdomain_add.inc.php">
-        <input type="text" placeholder="Domain"/>
-        <input type="text" placeholder="IP (IPv4)"/>
-        <input type="number" placeholder="TTL"/>
-        <button class="btn waves-effect waves-light" type="submit" name="action">Submit request
-            <i class="material-icons right">send</i>
-        </button>
-    </form> -->
-    <div class="input_field card-panel  ">
-        <h5 id="picture-add">Request a domain here:</h5>
+    <div class="form_div">
+        <h5 class="form_header">Request a domain here:</h5>
         <form action="/media/includes/subdomain_add.inc.php" method="POST">
 
             <div class="input-field">
-                <input type="text" id="subdomain" name="subdomain" data-length="50">
-                <label for="subdomain">Subdomain</label>
+                <input type="text" id="subdomain" name="subdomain" placeholder="Subdomain" data-length="50">
+                <!--<label for="subdomain">Subdomain</label>-->
             </div>
             <div class="input-field">
-                <input type="text" id="IP" name="IP">
-                <label for="IP">IP (IPv4)</label>
+                <input type="text" id="IP" name="IP" placeholder="IP (IPv4)">
+                <!--<label for="IP">IP (IPv4)</label>-->
             </div>
 
             <button class="btn waves-effect waves-light" type="submit" name="action">Request domain
