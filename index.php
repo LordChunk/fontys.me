065be4e0dd103df2b4a4b6fac7a8a0dea58e5989
LordChunk <LordChunk@users.noreply.github.com>
2017-09-28 15:26:17 +0200

    Tested some API settings

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
 <div class="container">
     <section class="block">
         <h2>Block 1</h2>
+        <p>
+            <?php
+                include "media/includes/FHICT_api.inc.php";
+
+                $service = new FHICTService();
+
+                $data = $service->getServiceData('/groups');
+
+                var_dump($data);
+            ?>
+        </p>
     </section>
     <section class="block">
         <h2>Block 1</h2>
---
+++ media/includes/FHICT_api.inc.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Job
+ * Date: 28/09/2017
+ * Time: 14:41
+ */
+//if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class FHICTService {
+    const BASE_URL = 'https://api.fhict.nl/';
+
+    public function __construct() {
+
+    }
+
+    public function getServiceData($servicePath) {
+        $url = self::BASE_URL . $servicePath;
+        $authorization = "Authorization: Bearer " . $this->getBearerToken();
+        $curl = curl_init ($url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array($authorization));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($curl);
+        $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        if($httpcode != 200) {
+            $this->login();
+        }
+        return json_decode($result);
+
+    }
+
+    protected function getBearerToken() {
+        /*if (isset($_SESSION['access_token']) && $_SESSION['access_token'] != '') {
+            $accessToken = $_SESSION['access_token'];
+        } else {
+            $this->login();
+        }
+        return $accessToken;
+        */
+
+        //Temp token
+        //return "";
+    }
+
+    protected function login() {
+        header("location: media/includes/login.inc.php");
+    }
+}
+
+
+/*
+ * include 'FHICTService';
+ *
+ * $service = new FHICTService();
+ * $data = $service->getServiceData('/groups);
+ *
+ * //
+ */
