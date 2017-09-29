b3600986336f4e1262b559fb89bbca6daec1089c
LordChunk <LordChunk@users.noreply.github.com>
2017-09-29 10:58:46 +0200

    Updated some canvas to-do list stuff

--- index.php
+++ index.php
@@ -1,35 +1,36 @@
 <?php
-    //include "media/includes/head.inc.php";
-    session_start();
+    include "media/includes/head.inc.php";
+    include "media/includes/FHICT_api.inc.php";
+    //session_start();
 ?>
-<!-- Body
+<!-- Body -->
 <div class="container">
     <section class="block">
-        <h2>Block 1</h2>
+        <h2>Canvas to-do list:</h2>
         <p>
+            <?php
+            /*
+             * Canvas to-do list
+             */
 
+            $service = new FHICTService();
+            $data = $service->getServiceData('/canvas/todo/me');
+            //$dump = $data[0]->{"assignment"}->{"name"};
+            //var_dump($dump);
+            $i = 0;
+            foreach ($data as $course)
+            {?>
+                <?=$data[$i]->{"assignment"}->{"name"} . "<br>"?>
+                <?php$i++;
+            }
+            ?>
         </p>
     </section>
     <section class="block">
-        <h2>Block 1</h2>
+        <h2>Block 2</h2>
     </section>
 </div>
 </body>
 </html>
--->
-<?php
-include "media/includes/FHICT_api.inc.php";
 
-$service = new FHICTService();
 
-$data = $service->getServiceData('/canvas/todo/me');
-$dump = $data[0]->{"assignment"}->{"name"};
-//var_dump($dump);
-
-$i = 0;
-foreach ($data as $course)
-{
-    echo $data[$i]->{"assignment"}->{"name"} . "<br>";
-    $i++;
-}
-?>
