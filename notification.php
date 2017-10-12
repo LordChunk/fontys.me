eb9d9d1f785d4913217316bfb5adb92c3b49a224
LordChunk <LordChunk@users.noreply.github.com>
2017-10-12 14:43:08 +0200

    Updated nav bar height animation

--- notification.php
+++ notification.php
@@ -8,64 +8,72 @@
     <script async src="media/js/notification.js"></script>
 </head>
 <body>
-<!--    <div class="container">-->
-<!--        <section>-->
-<!--            <h2>Onesignal test page</h2>-->
-<!---->
-<!--            <p style="-webkit-user-select: none" onclick="notifyMe();">Join the bell squad</p>-->
-<!--        </section>-->
-<!--    </div>-->
+<?php
 
+?>
+    <form method="post" action="notification.php">
+        <input name="body" placeholder="body">
+        <input name="title" placeholder="title">
+
+
+    </form>
     <?PHP
-    function sendMessage(){
-        $content = array(
-            "en" => 'Body enzo'
-        );
-        $headings = array(
-            'en' => "Titel"
-        );
+    if ($_POST["send"]){
 
-        $fields = array(
-            'app_id' => "d42d6e98-3d75-4968-bdf0-1cb00817fba3",
-            'included_segments' => array('All'),
-            //'data' => array("foo" => "bar"),
-            'contents' => $content,
-            "headings" => $headings,
-            //'send_after' => "2017-10-13 14:00:00 ". " (CEST)"
-        );
+        //Get data from api
+        include "media/includes/FHICT_api.inc.php";
 
-        $fields = json_encode($fields);
-        echo("<br>JSON sent:<br><br>");
-        echo($fields);
+        $service = new FHICTService();
+        $data = $service->getServiceData('/canvas/todo/me');
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-            'Authorization: Basic ZGU0NTU3ZTktMGJhNC00ZDMwLThmZTItNTFlNWRkYWY2MjJm'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
-        $response = curl_exec($ch);
-        curl_close($ch);
 
-        return $response;
+        //Build message
+        function sendMessage(){
+            $content = array(
+                "en" => 'Body enzo'
+            );
+            $headings = array(
+                'en' => "Titel"
+            );
+
+            $fields = array(
+                'app_id' => "d42d6e98-3d75-4968-bdf0-1cb00817fba3",
+                'included_segments' => array('All'),
+                //'data' => array("foo" => "bar"),
+                'contents' => $content,
+                "headings" => $headings,
+                //'send_after' => "2017-10-13 14:00:00 ". " (CEST)"
+            );
+
+            $fields = json_encode($fields);
+            echo("<br>JSON sent:<br><br>");
+            echo($fields);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+                'Authorization: Basic ZGU0NTU3ZTktMGJhNC00ZDMwLThmZTItNTFlNWRkYWY2MjJm'));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_HEADER, FALSE);
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            return $response;
+        }
+
+        $response = sendMessage();
+        $return["allresponses"] = $response;
+        $return = json_encode( $return);
+
+        echo("<br><br>JSON received:<br><br>");
+        echo($return);
     }
 
-    $response = sendMessage();
-    $return["allresponses"] = $response;
-    $return = json_encode( $return);
-
-    echo("<br><br>JSON received:<br><br>");
-    echo($return);
-
-
-
-    echo "<br><br><br>Test stuff: <br>";
-
-    var_dump(date_default_timezone_get());
     ?>
 
 </body>
