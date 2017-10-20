d5ebd7eee955fcafd9046b7118239f13a3978522
LordChunk <LordChunk@users.noreply.github.com>
2017-10-20 15:51:39 +0200

    Redid an unwanted merge

--- media/includes/late.inc.php
+++ media/includes/late.inc.php
@@ -6,82 +6,19 @@
  * Time: 21:23
  */
 session_start();
-$UID = $_SESSION["UID"];
-//Check when last email was send
-require "connect-Freaze.inc.php";
-
-//Create template
-$sql = "SELECT * FROM late_email WHERE UID=" . $UID;
-
-$result = mysqli_query($conn, $sql);
-
-if (!$result){
-    echo "an error occured<br>";
-    header('Location: /late?error=db');
-    exit();
-}
-
-$num_rows = mysqli_num_rows($result);
-$row = mysqli_fetch_row($result)[0];
-if ($num_rows == 0)
-{
-
-}
-elseif ($num_rows == 1)
-{
-    $last_sent = mysqli_fetch_row($result)[1]; //Get last send email time
-    if (time()+ (12 * 60 * 60) < $last_sent)    //Check for within 12 hours
-    {
-        //Within 12 hours
-        echo 'Wow no emails pls';
-        exit();
-    }
-
-    //Insert new time
-    $sql = "DELETE FROM late_email WHERE UID=".$UID.";
-            INSERT INTO late_email (UID, last_sent) VALUES (".$UID.", ".time().");";
-
-    $result = mysqli_query($conn, $sql);
-
-    if (!$result){
-        echo "an error occured<br>";
-        header('Location: /late?error=db');
-        exit();
-    }
-}
-else
-{
-    //header("location : /late?error=mutli_row");
-    var_dump($row);
-    exit();
-}
-
-
-
-
-exit();
-
-
 require $_SERVER["DOCUMENT_ROOT"] . "/media/includes/FHICT_api.inc.php";
 $service = new FHICTService();
-
 //Fetch today's schedule
 $schedule = $service->getServiceData('/schedule/me');
-
-
 //get first lesson and teacherAbbreviation
 $teacher = $schedule->{"data"}[0]->{"teacherAbbreviation"};
-
 $searchResult = $service->getServiceData('people/search/' . $teacher);
-
-
 /*
  * Check for failed requests
  * failed requests are mostly called by outdated session credentials.
  * This is a bug which will be fixed in a later version since it is quite complicated and is an issue
  * which only occurs if a user hasn't closed their browser for quite a long time
  */
-
 if ($searchResult)
 {
     foreach ($searchResult as $member)
@@ -98,17 +35,13 @@ else
     header("location: /late?error=login_timeout");
     exit();
 }
-
 //Define student var
 $student_email = $_SESSION['email'];
 $student_name = $_SESSION["name"];
-
 //Define reason and time vars
 $reason = htmlspecialchars($_POST["reason"]);
 $time = htmlspecialchars($_POST["time"] . "");
-
 //Check if required info isn't empty
-
 if (!$student_name || !$student_email)
 {
     header("location: /late?error=email_name");
@@ -117,9 +50,7 @@ if (!$student_name || !$student_email)
     header("Location: /late?error=empty_time");
     exit();
 }
-
 //Old email fetch method
-
 ////Get body var
 //if(include "mail/email_body.inc.php")
 //{
@@ -130,8 +61,6 @@ if (!$student_name || !$student_email)
 //    header("location: /late?error=body_fetch");
 //    exit();
 //}
-
-
 //New email fetch method
 $data = [
     "teacher_name" => $teacher_name,
@@ -140,21 +69,15 @@ $data = [
     "reason" => $reason,
     "time" => $time
 ];
-
 //Check for https
 $prefix = $_SERVER['HTTPS'] ? 'https://' : 'http://';
-
 //Direct path
 $ch = curl_init($prefix . $_SERVER['HTTP_HOST']. '/media/includes/email_body.inc.php');
-
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_USE_SSL, true);
-
-
 $email_body = curl_exec($ch);
-
 if($email_body)
 {
     echo "email body loaded properly <br>"; //You have to output something and !include throws and error
@@ -164,9 +87,6 @@ else
     header("location: /late?error=body_fetch");
     exit();
 }
-
-
-
 //Fetch credentials
 if(include "mail/mail_credentials.inc.php")
 {
@@ -178,24 +98,15 @@ else
     exit();
 }
 //Setup email
-
 $mail->Body = $email_body;
-
 echo "passed body loader";
-
-
 $mail->Subject = "Te laat melding: ". $student_name;
-
 //$mail->addAddress("j.vanooik@student.fontys.nl");
 //$mail->addAddress("meowingdalmatian@protonmail.com");
-$mail->addAddress($teacher_email);
+//$mail->addAddress($teacher_email);
 $mail->addAddress($student_email);
-
-
 //final
 //$mail->addAddress($teacher_address);
-
-
 //Send email
 if($mail->send())
 {
@@ -207,8 +118,5 @@ else
     header("location: /late?error=email_sender");
     exit();
 }
-
-
 echo "<br><br><br>We made it !<br>";
-
 header("location: /late?error=success");
