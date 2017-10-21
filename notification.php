44031023e6ec8cf3cf857a5e488aaf3d3bba1469
LordChunk <LordChunk@users.noreply.github.com>
2017-10-21 20:01:25 +0200

    Notifications are now send 1 day before and 1 week before a task is due. You'll also still get a notification when the project is due as well.

--- notification.php
+++ notification.php
@@ -11,98 +11,101 @@
 <?php
 //Notificatie: 1 dag en 1 week
 ?>
-    <form method="post" action="notification.php">
-        <input name="body" placeholder="body">
-        <input name="title" placeholder="title">
-        <input type="hidden" name="send" value="true">
-        <input type="submit">
-    </form>
-    <?PHP
-    //if ($_POST["send"] || true){
-        dueDateNotification();
+<h1>Notification scheduler:</h1>
+<?php
+session_start();
+dueDateNotification();
 
-        function dueDateNotification()
+function dueDateNotification()
+{
+    //Get data from api
+    include "media/includes/FHICT_api.inc.php";
+
+    $service = new FHICTService();
+    $data = $service->getServiceData('/canvas/upcoming/me');
+
+    //Get list of due dates
+    foreach ($data as $task)
+    {
+        //Check if due date is set
+        if($task->{"assignment"}->{"due_at"})
         {
-            session_start();
-            //Get data from api
-            include "media/includes/FHICT_api.inc.php";
+            $dueDate = $task->{"assignment"}->{"due_at"};
+            //Parse to make usable
+            $dueDate = str_replace("T", " ", $dueDate);
+            $dueDate = str_replace("Z", " ", $dueDate) . " (CEST)";
 
-            $service = new FHICTService();
-            $data = $service->getServiceData('/canvas/upcoming/me');
+            //$dueDate = null;    //Set time to right now for testing
 
+            $title = "⏰".$task->{"title"};
+            $body = "The following task is due right now: " . $task->{"title"} . ".\n\n Click this here to go to the turn in page.";
 
-            //Get list of due dates
-            foreach ($data as $task) {
-                //Check if due date is set
-                if($task->{"assignment"}->{"due_at"})
-                {
-                    $datum = $task->{"assignment"}->{"due_at"};
-                    //Parse to make usable
-                    $datum = str_replace("T", " ", $datum);
-                    $datum = str_replace("Z", " ", $datum) . " (CEST)";
+            $url = $task->{"html_url"};
 
-                    //$datum = null;    //Set time to right now for testing
-
-
-                    $title = "⏰".$task->{"title"};
-                    $body = "The following task is due right now: " . $task->{"title"} . ".\n\n Click this here to go to the turn in page.";
-
-                    $url = $task->{"html_url"};
-                    //Execute notification
-                    $response = sendMessage($title, $body, $datum, $url);
-                    $return["allresponses"] = $response;
-                    $return = json_encode($return);
-
-                    echo("<br><br>JSON received:<br><br>");
-                    echo($return);
-                }
+            //Loop once for every notification time (when due, 1 day before and 1 week before
+            $i = 0;
+            while($i < 3)
+            {
+                $notificationDates = array(
+                    $dueDate,
+                    date('Y-m-d H:i:s (T)', strtotime($dueDate. "-1 day")),
+                    date('Y-m-d H:i:s (T)', strtotime($dueDate. "-1 week"))
+                );
+                //Execute notification
+                $response = sendMessage($title, $body, $notificationDates[$i], $url);
+                $i++;
             }
         }
+    }
+}
 
-        //Build message
-        function sendMessage($title, $body, $time, $url){
+//Build message
+function sendMessage($title, $body, $time, $url){
 
-            //Convert into usable objects
-            $body = array(
-                "en" => $body
-            );
-            $title = array(
-                'en' => $title
-            );
+    //Convert into usable objects
+    $body = array(
+        "en" => $body
+    );
+    $title = array(
+        'en' => $title
+    );
 
-            //Build fields
-            $fields = array(
-                'app_id' => "d42d6e98-3d75-4968-bdf0-1cb00817fba3",
-                //This will be made group-specific in the future
-                'included_segments' => array('All'),
-                'contents' => $body,
-                "headings" => $title,
-                'send_after' => $time,
-                'url' => $url
-            );
+    //Build fields
+    $fields = array(
+        'app_id' => "d42d6e98-3d75-4968-bdf0-1cb00817fba3",
+        //This will be made group-specific in the future
+        'included_segments' => array('All'),
+        'contents' => $body,
+        "headings" => $title,
+        'send_after' => $time,
+        'url' => $url
+    );
 
-            $fields = json_encode($fields);
-            echo("<br>JSON sent:<br><br>");
-            echo($fields);
+    $fields = json_encode($fields);
+    echo("<br>JSON sent:<br>");
+    echo($fields . "<br>");
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-                                                                'Authorization: Basic ZGU0NTU3ZTktMGJhNC00ZDMwLThmZTItNTFlNWRkYWY2MjJm'));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            curl_setopt($ch, CURLOPT_HEADER, FALSE);
-            curl_setopt($ch, CURLOPT_POST, TRUE);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+                                                        'Authorization: Basic ZGU0NTU3ZTktMGJhNC00ZDMwLThmZTItNTFlNWRkYWY2MjJm'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
-            $response = curl_exec($ch);
-            curl_close($ch);
+    $response = curl_exec($ch);
+    curl_close($ch);
 
-            return $response;
-        }
-    //}
+    $return["allresponses"] = $response;
+    $return = json_encode($return);
 
-    ?>
+    echo("<br><br>JSON received:<br>");
+    echo($return . "<br>");
 
+    return $response;
+}
+?>
 </body>
 </html>
